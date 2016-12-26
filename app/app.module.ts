5cee5359f2519af40c0021ab3e59db702f3d775e
bijay <bijay48828@gmail.com>
2016-12-26 23:36:45 +0545

    quickstart

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,31 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }  from './dashboard.component'
+
+import { HeroService }         from './hero.service';
+import {AppRoutingModule}      from './app-routing.module'
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  
+  declarations: [
+    DashboardComponent,
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
